d88d650f26282684c96ec051201a0cd0449aab0c
Alya <upsale.agr@gmail.com>
2020-09-22 22:45:27 +0300

    #Stage 2/5

---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,9 @@
+#Stage 2/5: The user is the gamemaster
+field = input("Enter cells:")
+field_fringe = "---------"
+
+print(field_fringe)
+print('|' field[0], field[1], field[2], '|')
+print('|' field[3], field[4], field[5], '|')
+print('|' field[6], field[7], field[8], '|')
+print(field_fringe)
